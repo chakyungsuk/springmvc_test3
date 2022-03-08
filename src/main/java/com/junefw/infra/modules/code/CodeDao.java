f2433bb0bca6bd4428fbd0e078c0e1fde9edb580
chakyungsuk <cha0054@naver.com>
2022-03-08 10:33:00 +0900

    asd

--- src/main/java/com/junefw/infra/modules/code/CodeDao.java
+++ src/main/java/com/junefw/infra/modules/code/CodeDao.java
@@ -16,11 +16,15 @@ public class CodeDao {
 	
 	private static String namespace = "com.junefw.infra.modules.code.CodeMpp";
 
+	//fdcodeGroup
 	public List<Code> selectList(){ return sqlSession.selectList(namespace + ".selectList", "");}
-	
 	public int insert(Code dto) {return sqlSession.insert(namespace + ".insert", dto);}
-
 	public Code selectOne(CodeVo vo) {return sqlSession.selectOne(namespace + ".selectOne", vo);}
-	
 	public int update(Code dto) {return sqlSession.update(namespace + ".update", dto);}
+	
+	//fdcode
+	public List<Code> selectListCode(){ return sqlSession.selectList(namespace + ".selectListCode", "");}
+	public int insertCode(Code dto) {return sqlSession.insert(namespace + ".insertCode", dto);}
+	public Code selectOneCode(CodeVo vo) {return sqlSession.selectOne(namespace + ".selectOneCode", vo);}
+	public int updateCode(Code dto) {return sqlSession.update(namespace + ".updateCode", dto);}
 }
--- src/main/java/com/junefw/infra/modules/code/CodeService.java
+++ src/main/java/com/junefw/infra/modules/code/CodeService.java
@@ -4,11 +4,15 @@ import java.util.List;
 
 public interface CodeService {
 	
+	//fdcodeGroup
 	public List<Code> selectList() throws Exception; 
-
 	public int insert(Code dto) throws Exception;
-
 	public Code selectOne(CodeVo vo) throws Exception;
-	
 	public int update(Code dto) throws Exception;
+	
+	//fdcode
+	public List<Code> selectListCode() throws Exception; 
+	public int insertCode(Code dto) throws Exception;
+	public Code selectOneCode(CodeVo vo) throws Exception;
+	public int updateCode(Code dto) throws Exception;
 }
