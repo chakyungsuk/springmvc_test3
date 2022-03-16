03931a2ce91b94b4282828b2ce39a3e9db394269
chakyungsuk <cha0054@naver.com>
2022-03-16 13:48:30 +0900

    gh

--- src/main/java/com/junefw/infra/modules/code/CodeController.java
+++ src/main/java/com/junefw/infra/modules/code/CodeController.java
@@ -107,14 +107,22 @@ public class CodeController {
 	
 	@RequestMapping(value = "/code/codeList")
 	
-	public String codeList(CodeVo vo, Model model) throws Exception {
+
+	public String codeList(@ModelAttribute("vo") CodeVo vo, Model model) throws Exception {
 	
+		int count = service.selectOneCountCode(vo);
+		
+		vo.setParamsPaging(count);
+		
+		if (count != 0) {
 		List<Code> list = service.selectListCode(vo);
 		model.addAttribute("list", list);
 		
 		List<Code> listCodeGroup = service.selectList(vo);
 		model.addAttribute("listcodeGroup", listCodeGroup);
-		
+		} else {
+			
+		}
 		return "code/codeList";
 	}
 	
