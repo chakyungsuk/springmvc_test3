9b5db8a50c11c1e928198e6979977c405e0a3d1b
chakyungsuk <cha0054@naver.com>
2022-03-21 17:39:08 +0900

    asdf

--- src/main/java/com/junefw/infra/modules/member/MemberDao.java
+++ src/main/java/com/junefw/infra/modules/member/MemberDao.java
@@ -18,9 +18,14 @@ public class MemberDao {
 	
 	public int selectOneMember(MemberVo vo) {return sqlSession.selectOne(namespace + ".selectOneMember", vo);}
 	public List<Member> selectList(MemberVo vo){ return sqlSession.selectList(namespace + ".selectList", vo);}
+	
 	public int insert(Member dto){ return sqlSession.insert(namespace + ".insert", dto);}
 	public int insertEmail(Member dto){ return sqlSession.insert(namespace + ".insertEmail", dto);}
 	public int insertMobile(Member dto){ return sqlSession.insert(namespace + ".insertMobile", dto);}
+	
 	public Member selectOne(MemberVo vo) {return sqlSession.selectOne(namespace + ".selectOne", vo);}
+	
 	public int update(Member dto) {return sqlSession.update(namespace + ".update", dto);}
+	public int updateEmail(Member dto) {return sqlSession.update(namespace + ".updateEmail", dto);}
+	public int updateMobile(Member dto) {return sqlSession.update(namespace + ".updateMobile", dto);}
 }
