61aa14aea64691020bfc8e7cb9be759f597cafac
chakyungsuk <cha0054@naver.com>
2022-04-06 18:25:42 +0900

    asd

--- src/main/java/com/junefw/infra/HomeController.java
+++ src/main/java/com/junefw/infra/HomeController.java
@@ -19,7 +19,7 @@ public class HomeController {
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
-	/**
+	/** 
 	 * Simply selects the home view to render by returning its name.
 	 */
 	@RequestMapping(value = "/", method = RequestMethod.GET)
